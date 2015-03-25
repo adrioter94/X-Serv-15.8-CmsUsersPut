4e4b5cf7ce42c523aac45b307b7ce324c29aae12
Adrian <adrioter94@hotmail.es>
2015-03-25 19:45:27 +0100

    Entrega Ejercicio

--- cms_users_put/views.py
+++ cms_users_put/views.py
@@ -12,7 +12,7 @@ def processCMS_Users(request, recurso):
         salida += "Logged in as " + request.user.username + ". "
         salida += "<a href='/admin/logout/'>Logout</a><br>"
     else:
-        salida += "Not logged in."
+        salida += "Not logged in. "
         salida += "<a href='/admin/login/'>Login</a><br>"
 
     if request.method == "GET":
@@ -32,5 +32,5 @@ def processCMS_Users(request, recurso):
             except:
                 return HttpResponseNotFound("Error")
         else:
-            return HttpResponseNotFound("No puedes modificarlo\
-                                         si no estas auntetificado")
+            return HttpResponseNotFound("No puedes modificarlo" +
+                                         " si no estas auntetificado")
